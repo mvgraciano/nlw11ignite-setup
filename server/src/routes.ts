67fecff22f6e143e15e4894cd3b4bddab2727f64
Marcus Graciano <marcusgraciano.ads@outlook.com>
2023-01-19 18:43:53 -0300

    Added dependency Zod

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,10 +1,23 @@
-import { FastifyInstance} from 'fastify'
+import { FastifyInstance } from 'fastify'
+import { z } from 'zod' //validations
+
 import { prisma } from './lib/prisma'
 
 export async function appRoutes(app: FastifyInstance) {
-    app.get("/hello", async () => {
-        const habits = await prisma.habit.findMany({})
-    
-        return habits
+    app.post("/habits", async (request) => {
+        // const {} = request.body
+        // const {} = request.params
+        // const {} = request.query
+
+        const createHabitBody = z.object({
+            title: z.string(),
+            weekDays: z.array(
+                z.number().min(0).max(6)
+            ) // [0(dom), 1(seg), 6(sab)]
+        })
+
+        const { title, weekDays } = createHabitBody.parse(request.body)
+
+        
     })
 }
