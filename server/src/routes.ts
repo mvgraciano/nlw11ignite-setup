3d5a01c112ed08a5ffaebf5b33d91ab3b205b46b
Marcus Graciano <marcusgraciano.ads@outlook.com>
2023-01-19 20:14:55 -0300

    Added route to get Habits by date

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -35,4 +35,49 @@ export async function appRoutes(app: FastifyInstance) {
             }
         })
     })
+
+    app.get("/day", async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+
+        const parsedDate = dayjs(date).startOf('day')
+        const weekDay = parsedDate.get('day')
+
+        // Todos hábitos possíveis do dia
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate()
+            },
+            include: {
+                dayHabits: true
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => {
+            return dayHabit.habit_id
+        })
+
+        return {
+            possibleHabits,
+            completedHabits
+        }
+
+        // Hábitos já completados
+    })
 }
